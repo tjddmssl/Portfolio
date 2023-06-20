9be189835fc84e64b2a250a1d47b3e582447aa69
tjddmssl <115976700+tjddmssl@users.noreply.github.com>
2023-06-21 02:53:55 +0900

    Layout (#1)
    * router 설정
    
    * resume 미리보기
    
    * header 반응형

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { LuFlower2 } from "react-icons/lu";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center">
+      <Link href="/">
+        <div className="flex items-center pl-3 pt-3">
+          <h1 className="text-2xl font-bold text-pink-500 ml-4">
+            {"SungEun's Portfolio"}
+          </h1>
+          <LuFlower2 className="text-3xl text-pink-500 max-sm:hidden" />
+        </div>
+      </Link>
+      <nav className="flex gap-4 mt-5 font-bold text-pink-500 text-lg p-3 mr-4">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/assets/resume.pdf" target="_blank">
+          Resume
+        </Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
